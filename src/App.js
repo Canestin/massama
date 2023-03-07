4ae3c1ef52bf15f31a7f65ce909ab6771c8f3582
RovaEncoder <abessolochrist30@gmail.com>
2023-03-07 22:22:29 +0100

    fix: component add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+import DCA from "./components/DCA/DCA";
 import Inscription from "./components/Inscription/Inscription";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Inscription />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<Inscription />
+			<DCA />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,32 @@
+import photo1 from "../images/Pics/1.jpeg";
+import photo2 from "../images/Pics/98.jpeg";
+import photo3 from "../images/Pics/99.jpeg";
+
+const infoMilieu = [
+	{
+		id: 1,
+		phopho: photo1,
+		nom: "KatiAa",
+		content:
+			"Mariée à un homme qui n'arrive pas à me satisfaire, je recherche un mec pour une aventure d’un soir avec un mec saura m’emmener au bonheur ! Relation discrète souhaitée.",
+		nb_phopho: 12,
+	},
+	{
+		id: 2,
+		phopho: photo2,
+		nom: "Xtraconjugale",
+		content:
+			"Je cherche à me faire caresser les seins par un bel homme qui aime mater. Je suis chaude et libre tous les jeudis soirs.",
+		nb_phopho: 8,
+	},
+	{
+		id: 3,
+		phopho: photo3,
+		nom: "Armel-78",
+		content:
+			"Je souhaite rencontrer un homme pour soirées libérées et coquines. Je cherche un mec qui n'a pas peur de faire de nouvelles expériences en toute discrétion.",
+		nb_phopho: 5,
+	},
+];
+
+export { infoMilieu };
