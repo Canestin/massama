d3c4571f065de4847316d557b57059cdf3d572bf
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-03-09 00:28:39 +0100

    style: change somethinfs

--- src/components/Profiles/Profiles.js
+++ src/components/Profiles/Profiles.js
@@ -2,23 +2,29 @@ import Navbar from "../Navbar/Navbar";
 import styles from "./Profiles.module.css";
 import { imageMap } from "../../images/utils/helpers";
 import { IoIosRadioButtonOn } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 export default function Profiles() {
 	const profiles = new Array(100).fill(0);
 	return (
 		<div className="Profiles">
 			<Navbar />
+
 			<div className={styles.profilesList}>
 				{profiles.map((_, i) => {
 					let img = (i + 1) % 50 !== 0 ? (i + 1) % 50 : 50;
-					return <User image={imageMap[img]} />;
+					return (
+						<Link to={`/conversations/${i + 1}`}>
+							<User key={`user-${i}`} image={imageMap[img]} id={i + 1} />
+						</Link>
+					);
 				})}
 			</div>
 		</div>
 	);
 }
 
-function User({ image }) {
+function User({ image, id }) {
 	return (
 		<div className={styles.profile}>
 			<div className={styles.onlineBtn}>
