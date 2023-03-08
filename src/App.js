c9a1cda1fa994c1c76e0cb303f4acf6ffb847e3e
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-03-08 17:22:32 +0100

    chore: add profiles component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import DCA from "./components/DCA/DCA";
 import Inscription from "./components/Inscription/Inscription";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Profiles from "./components/Profiles/Profiles";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Inscription />
-      <DCA />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<Inscription />
+			<DCA />
+			<Footer />
+
+			{/* <Profiles /> */}
+		</div>
+	);
 }
 
 export default App;
