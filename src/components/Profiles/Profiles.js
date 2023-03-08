4655ffc78ac56a6f4d14eb5c7ac64a2c7d431f62
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-03-08 17:17:07 +0100

    feat: finalize first version for profiles page

--- src/components/Profiles/Profiles.js
+++ src/components/Profiles/Profiles.js
@@ -1,20 +1,37 @@
 import Navbar from "../Navbar/Navbar";
+import styles from "./Profiles.module.css";
+import { imageMap } from "../../images/utils/helpers";
+import { IoIosRadioButtonOn } from "react-icons/io";
 
-function Profiles() {
+export default function Profiles() {
+	const profiles = new Array(100).fill(0);
 	return (
 		<div className="Profiles">
 			<Navbar />
-			<User />
+			<div className={styles.profilesList}>
+				{profiles.map((_, i) => {
+					let img = (i + 1) % 50 !== 0 ? (i + 1) % 50 : 50;
+					return <User image={imageMap[img]} />;
+				})}
+			</div>
 		</div>
 	);
 }
 
-export default Profiles;
-
-function User() {
+function User({ image }) {
 	return (
-		<div className="Profiles">
-			<Navbar />
+		<div className={styles.profile}>
+			<div className={styles.onlineBtn}>
+				<IoIosRadioButtonOn />
+			</div>
+			<img
+				style={{ objectFit: "cover", objectPosition: "top" }}
+				src={image}
+				alt="women"
+			/>
+			<div className={styles.infosprofile}>
+				<span>Sarah - 27 ans</span>
+			</div>
 		</div>
 	);
 }
