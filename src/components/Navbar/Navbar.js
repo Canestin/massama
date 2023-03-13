8a91dc9f8758507c74849e3ec19d5e9291292c00
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-03-13 23:41:11 +0100

    chore: import change

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import styles from "../Navbar/Navbar.module.css";
-import logo from "../../images/Pics/ph-logo.png";
+import logo from "../../images/others/ph-logo.png";
 
 export default function Navbar() {
-  return (
-    <div className={styles.container}>
-      <img alt="logo" className={styles.logo} src={logo}></img>
-      <div className={styles.member}> Déja Membre</div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<img alt="logo" className={styles.logo} src={logo}></img>
+			<div className={styles.member}> Déja Membre</div>
+		</div>
+	);
 }
