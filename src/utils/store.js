2d95d6ae5d31a3b5ca77c6753ea86f2b4f1a71e2
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-03-20 03:47:16 +0100

    feat:: handle fetch and matching data with routing

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import supabase from "./supabase";
+
+const loggedUserId = localStorage.getItem("userId");
 
 /**
  * @param {number} channelId the currently selected Channel
@@ -7,36 +10,40 @@ import supabase from "./supabase";
 export const useStore = (props) => {
 	const [channels, setChannels] = useState([]);
 	const [messages, setMessages] = useState([]);
-	const [users] = useState(new Map());
 	const [newMessage, setNewMessage] = useState(null);
 	const [newChannel, setNewChannel] = useState(null);
-	const [newOrUpdatedUser, handleNewOrUpdatedUser] = useState(null);
 	const [deletedChannel, handleDeletedChannel] = useState(null);
 
-	const loggedUserId = localStorage.getItem("userId");
+	const location = useLocation();
 
 	// Load initial data and set up listeners
 	useEffect(() => {
-		// Get Channels
-		fetchChannels(loggedUserId, setChannels);
+		// Get All Channels
+		fetchChannels(setChannels);
 
 		// Listen for new messages
 		const messageListener = supabase
 			.channel("public:messages")
 			.on(
 				"postgres_changes",
-				{ event: "INSERT", schema: "public", table: "messages" },
+				{
+					event: "INSERT",
+					schema: "public",
+					filter: `receiver_id=eq.${loggedUserId}`,
+					table: "messages",
+				},
+
 				(payload) => setNewMessage(payload.new)
 			)
-			.subscribe();
-
-		// Listen for changes to our users
-		const userListener = supabase
-			.channel("public:users")
 			.on(
 				"postgres_changes",
-				{ event: "*", schema: "public", table: "users" },
-				(payload) => handleNewOrUpdatedUser(payload.new)
+				{
+					event: "INSERT",
+					schema: "public",
+					filter: `sender_id=eq.${loggedUserId}`,
+					table: "messages",
+				},
+				(payload) => setNewMessage(payload.new)
 			)
 			.subscribe();
 
@@ -45,62 +52,86 @@ export const useStore = (props) => {
 			.channel("public:channels")
 			.on(
 				"postgres_changes",
-				{ event: "INSERT", schema: "public", table: "channels" },
-				(payload) => setNewChannel(payload.new)
+				{
+					event: "*",
+					schema: "public",
+					filter: `user1=eq.${loggedUserId}`,
+					table: "channels",
+				},
+				(payload) => {
+					if (payload.eventType === "INSERT") {
+						const channelInfos = getChannelsInfos([payload.new])[0];
+						setNewChannel(channelInfos);
+					} else if (payload.eventType === "UPDATE") {
+						const updatedChannels = channels.sort(function (a, b) {
+							return new Date(b.lastMessageDate) - new Date(a.lastMessageDate);
+						});
+						setChannels(updatedChannels);
+					}
+				}
 			)
 			.on(
 				"postgres_changes",
-				{ event: "DELETE", schema: "public", table: "channels" },
-				(payload) => handleDeletedChannel(payload.old)
+				{
+					event: "*",
+					schema: "public",
+					filter: `user2=eq.${loggedUserId}`,
+					table: "channels",
+				},
+				(payload) => {
+					handeChannelsChanges(
+						payload,
+						location.state,
+						setNewChannel,
+						setChannels
+					);
+				}
 			)
+			// .on(
+			// 	"postgres_changes",
+			// 	{ event: "DELETE", schema: "public", table: "channels" },
+			// 	(payload) => handleDeletedChannel(payload.old)
+			// )
 			.subscribe();
-		// Cleanup on unmount
+
 		return () => {
 			supabase.removeChannel(messageListener);
-			supabase.removeChannel(userListener);
 			supabase.removeChannel(channelListener);
 		};
 	}, []);
 
+	useEffect(() => {
+		if (props?.channelId === "new-channel") {
+			const existingChannel = channels.find(
+				(channel) => channel.speaker.id === location.state.id
+			);
+
+			if (existingChannel) fetchMessages(existingChannel.id, setMessages);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [channels]);
+
 	// Update when the route changes
 	useEffect(() => {
-		if (!!props?.channelId) {
-			// fetchMessages(props.channelId, (messages) => {
-			// 	messages.forEach((x) => users.set(x.user_id, x.author));
-			// 	setMessages(messages);
-			// 	alert("messages " + messages)
-			// });
+		if (!!props?.channelId && props?.channelId !== "new-channel") {
 			fetchMessages(props.channelId, setMessages);
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [props.channelId]);
+	}, [props?.channelId]);
 
 	// New message received from Postgres
 	useEffect(() => {
-		// if (newMessage && newMessage.channel_id === Number(props.channelId)) {
-		// 	const handleAsync = async () => {
-		// 		let authorId = newMessage.user_id;
-		// 		if (!users.get(authorId))
-		// 			await fetchUserById(authorId, (user) => handleNewOrUpdatedUser(user));
-		// 		setMessages(messages.concat(newMessage));
-		// 	};
-		// 	handleAsync();
-		// }
-
-		if (!!props?.channelId) {
-			// fetchMessages(props.channelId, (messages) => {
-			// 	messages.forEach((x) => users.set(x.user_id, x.author));
-			// 	setMessages(messages);
-			// 	alert("messages " + messages)
-			// });
-			fetchMessages(props.channelId, setMessages);
+		if (!!props?.channelId && !!newMessage) {
+			setMessages(messages.concat(newMessage));
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [newMessage]);
 
 	// New channel received from Postgres
 	useEffect(() => {
-		if (newChannel) setChannels(channels.concat(newChannel));
+		if (newChannel) {
+			setChannels([newChannel].concat(channels));
+		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [newChannel]);
 
@@ -113,21 +144,16 @@ export const useStore = (props) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [deletedChannel]);
 
-	// New or updated user received from Postgres
-	useEffect(() => {
-		if (newOrUpdatedUser) users.set(newOrUpdatedUser.id, newOrUpdatedUser);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [newOrUpdatedUser]);
-
 	return {
-		// We can export computed values here to map the authors to each message
-		// messages: messages.map((x) => ({ ...x, author: users.get(x.user_id) })),
-		messages: messages,
+		messages,
 		channels:
 			channels !== null
-				? channels.sort((a, b) => a.slug.localeCompare(b.slug))
+				? channels.sort(
+						(a, b) =>
+							new Date(a.last_message_sent_at) -
+							new Date(b.last_message_sent_at)
+				  )
 				: [],
-		users,
 	};
 };
 
@@ -151,17 +177,34 @@ export const fetchProfiles = async (setState) => {
  * Fetch all channels
  * @param {function} setState Optionally pass in a hook or callback to set the state
  */
-export const fetchChannels = async (userId, setState) => {
+export const fetchChannels = async (setState) => {
 	try {
-		let { data } = await supabase
+		const { data } = await supabase
 			.from("channels")
-			.select(
-				`user1(id, username, avatar, city, age), user2(id, username, avatar, city, age)`
-			)
-			.overlaps("users", [userId]);
-		if (setState) setState(data);
-		// return data;
+			.select("*, user1(*), user2(*)")
+			.or(`user1.eq.${loggedUserId}, user2.eq.${loggedUserId}`)
+			.order("last_message_sent_at", { ascending: false });
+
+		const channels = getChannelsInfos(data);
+
+		if (setState) setState(channels);
 	} catch (error) {
+		console.log("error", error);
+	}
+};
+
+/**
+ * Update  channel when a message is sent
+ * @param {function} setState Optionally pass in a hook or callback to set the state
+ */
+export const updateChannel = async (channel_id, last_message) => {
+	try {
+		await supabase
+			.from("channels")
+			.update({ last_message_sent_at: new Date(), last_message })
+			.eq("id", channel_id);
+	} catch (error) {
+		alert("Erreur lors de la mise à jour du channel !");
 		console.log("error", error);
 	}
 };
@@ -179,6 +222,7 @@ export const fetchUserById = async (userId, setState) => {
 			.eq("id", userId)
 			.single();
 		if (setState) setState(data);
+		else return data;
 	} catch (error) {
 		console.log("error", error);
 	}
@@ -186,17 +230,84 @@ export const fetchUserById = async (userId, setState) => {
 
 /**
  * Fetch a single user - OKAY -
- * @param {string} username
+ * @param {string[]} channels
+ * @param {string} channelId
  * @param {function} setState Optionally pass in a hook or callback to set the state
  */
-export const fetchUserByUsername = async (username, setState) => {
+export const getUserFromChannels = (channels, channelId, setState) => {
+	if (!!channels && !!channelId) {
+		const channel = channels.find((c) => c.id === channelId);
+		setState(channel?.speaker);
+	}
+};
+
+/**
+ * Fetch all messages and their authors
+ * @param {number} channelId
+ * @param {function} setState Optionally pass in a hook or callback to set the state
+ */
+export const fetchMessages = async (channelId, setState) => {
 	try {
 		let { data } = await supabase
-			.from("profiles")
+			.from("messages")
 			.select(`*`)
-			.eq("username", username)
-			.single();
+			.eq("channel_id", channelId)
+			.order("sent_at", { ascending: true });
+
 		if (setState) setState(data);
+	} catch (error) {
+		console.log("error", error);
+	}
+};
+/**
+ * Insert a new channel into the DB
+ * @param {string} slug The channel name
+ * @param {number} user_id The channel creator
+ */
+export const addChannel = async (user1, last_message) => {
+	try {
+		const { data } = await supabase
+			.from("channels")
+			.insert([{ user1, user2: loggedUserId, last_message }])
+			.select("id");
+		return data[0].id;
+	} catch (error) {
+		alert("Erreur lors de la creation du channel !");
+	}
+};
+
+/**
+ * Insert a new message into the DB
+ * @param {string} message The message text
+ * @param {number} channel_id The receiver
+ * @param {number} author_id The author
+ */
+export const addMessage = async (
+	content,
+	sender_id,
+	receiver_id,
+	channel_id
+) => {
+	try {
+		await supabase
+			.from("messages")
+			.insert([{ content, sender_id, receiver_id, channel_id }]);
+	} catch (error) {
+		alert("Erreur lors de l'envoi du message !");
+	}
+};
+
+/**
+ * Delete a channel from the DB
+ * @param {number} channel_id
+ */
+export const deleteChannel = async (channel_id) => {
+	try {
+		let { data } = await supabase
+			.from("channels")
+			.delete()
+			.match({ id: channel_id });
+		return data;
 	} catch (error) {
 		console.log("error", error);
 	}
@@ -217,77 +328,52 @@ export const fetchUserRoles = async (setState) => {
 };
 
 /**
- * Fetch all messages and their authors
- * @param {number} channelId
- * @param {function} setState Optionally pass in a hook or callback to set the state
+ * Retrieve well-organized list of channels
+ * @param {object[]} data Pass the list of confused channel objects
  */
-export const fetchMessages = async (channelId, setState) => {
-	try {
-		let { data } = await supabase
-			.from("messages")
-			.select(`*, author:author_id(*)`)
-			.eq("channel_id", 1)
-			.order("created_at", true);
-		if (setState) setState(data);
-		return data;
-	} catch (error) {
-		console.log("error", error);
-	}
+export const getChannelsInfos = (data) => {
+	return data.map((channel) => {
+		return {
+			id: channel.id,
+			lastMessage: channel.last_message,
+			lastMessageDate: channel.last_message_sent_at,
+			speaker: getSpeaker("user1", "user2", channel),
+		};
+	});
 };
-/**
- * Insert a new channel into the DB
- * @param {string} slug The channel name
- * @param {number} user_id The channel creator
- */
-export const addChannel = async (userId1, userId2) => {
-	try {
-		let { data } = await supabase
-			.from("channels")
-			.insert([{ users: [userId1, userId2] }])
-			.select();
-		// console.log("channels de chez channels", data);
-		return data;
-	} catch (error) {
-		console.log("error", error);
+
+export const handeChannelsChanges = (
+	payload,
+	speaker,
+	setNewChannel,
+	setChannels
+) => {
+	if (payload.eventType === "INSERT") {
+		const newChannel = getChannelsInfos([payload.new])[0];
+		setNewChannel({ ...newChannel, speaker });
+	} else if (payload.eventType === "UPDATE") {
+		const updatedChannel = getChannelsInfos([payload.new])[0];
+
+		setChannels((channels) => {
+			const updatedChannelUserInfos = channels.find(
+				(c) => c.id === updatedChannel.id
+			).speaker;
+			const channelsWithoutUpdatedChannel = channels.filter(
+				(c) => c.id !== updatedChannel.id
+			);
+
+			return [
+				{ ...updatedChannel, speaker: updatedChannelUserInfos },
+				...channelsWithoutUpdatedChannel,
+			];
+		});
 	}
 };
 
-/**
- * Insert a new message into the DB
- * @param {string} message The message text
- * @param {number} channel_id
- * @param {number} user_id The author
- */
-export const addMessage = async (message, channel_id, author_id) => {
-	console.log("Infos avant envoi ::: ");
-	console.log("message : ", message);
-	console.log("channel_id : ", channel_id);
-	console.log("author_id : ", author_id);
-	try {
-		let { data } = await supabase
-			.from("messages")
-			.insert([{ message, channel_id: 1, author_id }])
-			.select()
-			.single();
-		// return data;
-		console.log("New message sent : ", data);
-	} catch (error) {
-		console.log("error", error);
-	}
-};
-
-/**
- * Delete a channel from the DB
- * @param {number} channel_id
- */
-export const deleteChannel = async (channel_id) => {
-	try {
-		let { data } = await supabase
-			.from("channels")
-			.delete()
-			.match({ id: channel_id });
-		return data;
-	} catch (error) {
-		console.log("error", error);
-	}
+const getSpeaker = (column1, column2, object) => {
+	const speaker = Object.values({
+		user1: object[column1],
+		user2: object[column2],
+	}).find((user) => user.id !== loggedUserId);
+	return speaker;
 };
