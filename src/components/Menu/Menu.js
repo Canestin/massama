27fda7cc7ec07f966213eba9a49ee5eca40206f9
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-04-05 02:43:46 +0200

    chore: adapt menu for mobile

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Menu.module.css";
-// import logo from "../../images/others/ph-logo.png";
 import { BsSearch, BsEye } from "react-icons/bs";
 import { TbMessageCircle, TbWallet, TbSettings, TbHelp } from "react-icons/tb";
 import { RiLogoutBoxLine } from "react-icons/ri";
@@ -9,61 +8,106 @@ import { useContext } from "react";
 import UserContext from "../../utils/UserContext";
 import { imageMap } from "../../utils/helpers";
 import { useStore } from "../../utils/store";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from "../../images/others/meetblack.svg";
 
-export default function Menu() {
+export default function Menu({ showMenu, setShowMenu }) {
 	const loggedUser = useContext(UserContext);
 	const { channels } = useStore();
+	const [windowSize, setWindowSize] = useState({
+		width: undefined,
+		height: undefined,
+	});
+
+	const toggleMenu = () => {
+		setShowMenu(!showMenu);
+		console.log("Menu Hidden !");
+	};
+	const channelId = !!channels[0] ? channels[0].id : "no-message";
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		}
+
+		window.addEventListener("resize", handleResize);
+
+		handleResize();
+
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
 
 	return (
-		<div className={styles.container}>
-			<div className={styles.logo}>
-				{/* <img src={logo} alt="logo" /> */}
-				<span style={{ color: "white", fontWeight: "bold", fontSize: 30 }}>
-					LOGO
-				</span>
-			</div>
-			<div className={styles.username}>
-				<img src={imageMap[loggedUser?.avatar]} alt="username" />
-				<div className={styles.usernameInfos}>
-					<span style={{ color: "white" }}>{loggedUser?.username}</span>
-					<br />
-					<span style={{ color: "gray" }}>Votre profil</span>
-				</div>
-			</div>
-			<div className={styles.listItems}>
-				<Link to="/feed">
-					<BsSearch color="#6438E1" size={28} />
-					<span>Parcourir</span>
-				</Link>
-				<Link to={`/conversations/${channels[0]?.id}`}>
-					<TbMessageCircle color="#6438E1" size={28} />
-					<span>Messages</span>
-				</Link>
-				<Link to="/views">
-					<BsEye color="#6438E1" size={28} />
-					<span>Vues</span>
-				</Link>
-				<Link to="/wallet">
-					<TbWallet color="#6438E1" size={28} />
-					<span>Portefeuille</span>
-				</Link>
-			</div>
-			<div className={`${styles.listItems} ${styles.settings}`}>
-				<Link to="/settings">
-					<TbSettings color="#6438E1" size={28} />
-					<span>Réglages</span>
-				</Link>
-				<Link to="/wallet">
-					<TbHelp color="#6438E1" size={28} />
-					<span>Aide</span>
-				</Link>
-			</div>
+		<div className={`${styles.menu} ${showMenu && styles.menuDisplayed}`}>
+			<div className={styles.container}>
+				{windowSize.width > 768 && (
+					<div className={styles.logo}>
+						<img src={logo} alt="logo" />
+					</div>
+				)}
 
-			<div className={`${styles.listItems} ${styles.logout}`}>
-				<Link to="/disconnect">
-					<RiLogoutBoxLine color="#6438E1" size={28} />
-					<span>Deconnexion</span>
-				</Link>
+				<div className={styles.username}>
+					<img src={imageMap[loggedUser?.avatar]} alt="username" />
+					<div className={styles.usernameInfos}>
+						<span style={{ color: "white" }}>{loggedUser?.username}</span>
+						<br />
+						<span style={{ color: "gray" }}>Votre profil</span>
+					</div>
+				</div>
+				<div className={styles.listItems}>
+					<Link to="/feed">
+						<BsSearch color="#6438E1" size={windowSize.width > 768 ? 28 : 22} />
+						<span>Parcourir</span>
+					</Link>
+					<Link to={`/conversations/${channelId}`}>
+						<TbMessageCircle
+							color="#6438E1"
+							size={windowSize.width > 768 ? 28 : 22}
+						/>
+						<span>Messages</span>
+					</Link>
+					{/* <Link to="/views">
+						<BsEye color="#6438E1" size={windowSize.width > 768 ? 28 : 22} />
+						<span>Vues</span>
+					</Link> */}
+					<Link to="/wallet">
+						<TbWallet color="#6438E1" size={windowSize.width > 768 ? 28 : 22} />
+						<span>Portefeuille</span>
+					</Link>
+				</div>
+				<div className={`${styles.listItems} ${styles.settings}`}>
+					<Link to="/settings">
+						<TbSettings
+							color="#6438E1"
+							size={windowSize.width > 768 ? 28 : 22}
+						/>
+						<span>Réglages</span>
+					</Link>
+					<Link to="/wallet">
+						<TbHelp color="#6438E1" size={windowSize.width > 768 ? 28 : 22} />
+						<span>Aide</span>
+					</Link>
+				</div>
+
+				<div className={`${styles.listItems} ${styles.logout}`}>
+					<Link to="/disconnect">
+						<RiLogoutBoxLine
+							color="#6438E1"
+							size={windowSize.width > 768 ? 28 : 22}
+						/>
+						<span>Deconnexion</span>
+					</Link>
+				</div>
+
+				<AiOutlineClose
+					size={25}
+					color="white"
+					className={styles.closeMenu}
+					onClick={toggleMenu}
+				/>
 			</div>
 		</div>
 	);
