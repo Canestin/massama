d2e6a1cd6cbffa03036b238225f0d2b235580828
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-04-21 16:26:57 +0200

    feat: add vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { inject } from "@vercel/analytics";
-inject();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
