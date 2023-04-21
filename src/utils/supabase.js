f4e767ff080b6ae6efd5fe46b4aa157b1851cfdf
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-04-21 14:04:15 +0200

    chore: change supabase vars

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,11 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = process.env.SUPABASE_URL;
-// const supabaseKey = process.env.SUPABASE_KEY;
-
-const supabaseUrl = "https://nrmxjtbclefxfyuxbhji.supabase.co";
-const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ybXhqdGJjbGVmeGZ5dXhiaGppIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg3NDM2NjgsImV4cCI6MTk5NDMxOTY2OH0.Vkdhou6voJHRMwp_ScZ5RQPYqAswoonTBTQyXLsBMW8";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
